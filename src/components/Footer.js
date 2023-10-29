62aa4be800fb2e5f20cf009aa84da6485899fa1d
Nurkanat <nurkanat.doszhan@gmail.com>
2023-10-29 18:19:33 +0600

    Timer через useEffect

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,26 +2,44 @@ import "./Footer.scss";
 // import the library
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { useEffect, useState } from "react";
 
 // const dateTime = () => {
-//   const currentdate = new Date();
-//   const currentTime = () => {
-//     let datetime = "" + currentdate.getHours() + ":"
-//     + ((currentdate.getMinutes().toString()).length === 2 ? currentdate.getMinutes() : '0'+currentdate
-//     .getMinutes()) + ":"
-//     + ((currentdate.getSeconds().toString()).length === 2 ? currentdate.getSeconds() : '0'+currentdate
-//     .getSeconds());
-//     return datetime;
-//   }
-//   let timerId = setInterval(currentTime, 1000);
-//   // setInterval(() => {
-//   //   return datetime;
-//   // }, 900);
-
-//   return timerId;
+//   let timerId
+//   setInterval(() => {
+//     timerId = h + ':' + m + ':' + s;
+//     if (h.toString().length == 1) {
+//       timerId = '0' + h + ':' + m + ':' + s;
+//     } else if (m.toString().length == 1) {
+//       timerId = h + ':' + '0' + m + ':' + s;
+//     } else if (s.toString().length == 1)  {
+//       timerId = h + ':' + m + ':' + s;
+//     }
+//     console.log(timerId)
+//     return timerId;
+//   }, 1000);
 // }
 
 const Footer = () => {
+
+  const [time, setTime] = useState()
+
+  useEffect(() => {
+    console.log('init')
+    let timerId
+    setInterval(() => {
+      const currentTime = new Date()
+      let h = currentTime.getHours()
+      let m = currentTime.getMinutes()
+      let s = currentTime.getSeconds()
+      timerId = h + ':' + m + ':' + s
+      setTime(timerId)
+      console.log(time)
+      // return time
+    }, 1000)
+  }, [time])
+
+  // console.warn(dateTime())
   return (
     <footer>
       <div className="container">
@@ -38,7 +56,7 @@ const Footer = () => {
             <small>© Nurkanat Doshan, 2023</small>
             <br />
             {/* <small>{dateTime()}</small> */}
-            <small>clock</small>
+            <small> clock</small>
           </div>
           <div className="col">
             <img className="logo" src="logow.webp" />
