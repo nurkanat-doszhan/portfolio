c54e5f58c0fea66a4ffee869bcae4127fc7b56f8
nurkanat-doszhan <nurkanat.doszhan@gmail.com>
2023-11-03 18:21:03 +0600

    Надо допилить страницу портфолио

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,22 @@
+import app from '../App.module.scss';
+import style from './Portfolio.module.scss';
+
 const Portfolio = () => {
   return (
-    <h1>Portfolio</h1>
+    <>
+      <div className={`${app.container} ${style.container}`}>
+        <h2>Portfolio</h2>
+        <div className={app.inner}>
+          <div className={app.col}>
+            <div className={style.flex}>
+              <div className={style.text}>
+                <h3>What Service You Will Get From Me</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
