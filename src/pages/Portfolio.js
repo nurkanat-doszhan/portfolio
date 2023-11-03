1d33df113642f914705b2b435a0d1b3fd0f8e835
Nurkanat <nurkanat.doszhan@gmail.com>
2023-11-04 01:07:31 +0600

    Добавил контент в портфолио

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
 import app from '../App.module.scss';
 import style from './Portfolio.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 
 const Portfolio = () => {
+  const [cards, setCards] = useState([
+    {
+      img: '/gallery/Leonardo_Diffusion_parting_rain_young_man_and_young_woman_man_3.jpg',
+      text: 'Hangman Game'
+    },
+    {
+      img: '/gallery/Leonardo_Diffusion_parting_rain_young_man_and_young_woman_man_3.jpg',
+      text: 'Hangman Game'
+    },
+    {
+      img: '/gallery/Leonardo_Diffusion_parting_rain_young_man_and_young_woman_man_3.jpg',
+      text: 'Hangman Game'
+    },
+    {
+      img: '/gallery/Leonardo_Diffusion_parting_rain_young_man_and_young_woman_man_3.jpg',
+      text: 'Hangman Game'
+    },
+    {
+      img: '/gallery/Leonardo_Diffusion_parting_rain_young_man_and_young_woman_man_3.jpg',
+      text: 'Hangman Game'
+    },
+  ])
   return (
     <>
       <div className={`${app.container} ${style.container}`}>
@@ -9,9 +34,23 @@ const Portfolio = () => {
         <div className={app.inner}>
           <div className={app.col}>
             <div className={style.flex}>
-              <div className={style.text}>
-                <h3>What Service You Will Get From Me</h3>
-              </div>
+              {
+                cards.map((i, v) => {
+                  return (
+                    <a key={v} className={style.card}>
+                      <div className={style.img}>
+                        <img src={i.img} />
+                        <div className={style.play}>
+                          <FontAwesomeIcon icon={faCirclePlay} />
+                        </div>
+                      </div>
+                      <div className={style.text}>
+                        <h4>{i.text}</h4>
+                      </div>
+                    </a>
+                  )
+                })
+              }
             </div>
           </div>
         </div>
