1f07926d85984cfbb78f9d652dbf03279c589cf7
Nurkanat <nurkanat.doszhan@gmail.com>
2024-03-25 15:55:53 +0500

    Фиксы

--- src/pages/projects/HangmanGame/HangmanGame.js
+++ src/pages/projects/HangmanGame/HangmanGame.js
@@ -3,9 +3,14 @@ import style from '../../projects/HangmanGame/HangmanGame.module.scss';
 import { useEffect, useState } from "react";
 
 const HangmanGame = () => {
+  const [word, setWord] = useState(null);
+  let w = [];
 
-  const [wordCount, setWordCount] = useState()
-  const [question, setQuestion] = useState([
+  useEffect(() => {
+    setWord(w)
+  }, [])
+
+  const [question] = useState([
     {q: 'A nocturnal flying mammal', a: 'bat'},
     {q: 'A sweet, sticky substance made by bees', a: 'honey'},
     {q: 'A cold-blooded reptile often kept as a pet', a: 'snake'},
@@ -19,14 +24,23 @@ const HangmanGame = () => {
 
   let randomQ = Math.floor(Math.random() * question.length);
 
-  useEffect(() => setWordCount(question[randomQ].a.length))
-
-  let createWord = () => {
-    let word = '';
-    for (let i = 0; i < wordCount; i++) {
-      console.log(randomQ)
+  const Letters = () => {
+    let letters = [];
+    for (let i = 0; i < question[randomQ].a.length; i++) {
+      w.push(question[randomQ].a[i]);
+      console.log(question[randomQ].a[i])
+      letters.push(<div className={style.letter}><div>{question[randomQ].a[i]}</div></div>);
     }
-    return wordCount
+    return letters;
+  }
+
+  const keyDown = (keyCode) => {
+    console.log(w)
+    for (let i = 0; i < question[randomQ].a.length; i++) {
+      keyCode == question[randomQ].a[i] ?
+      console.log(question[randomQ].a[i]) :
+      console.warn('#')
+    }
   }
 
   return (
@@ -51,36 +65,36 @@ const HangmanGame = () => {
               Question: { question[randomQ].q }
             </div>
             <div className={style.words}>
-              {createWord()}
+              <Letters />
             </div>
             <div className={style.keyboard}>
-              <button>a</button>
-              <button>b</button>
-              <button>c</button>
-              <button>d</button>
-              <button>e</button>
-              <button>f</button>
-              <button>g</button>
-              <button>h</button>
-              <button>i</button>
-              <button>j</button>
-              <button>k</button>
-              <button>l</button>
-              <button>m</button>
+              <button onClick={() => keyDown('a')}>a</button>
+              <button onClick={() => keyDown('b')}>b</button>
+              <button onClick={() => keyDown('c')}>c</button>
+              <button onClick={() => keyDown('d')}>d</button>
+              <button onClick={() => keyDown('e')}>e</button>
+              <button onClick={() => keyDown('f')}>f</button>
+              <button onClick={() => keyDown('g')}>g</button>
+              <button onClick={() => keyDown('h')}>h</button>
+              <button onClick={() => keyDown('i')}>i</button>
+              <button onClick={() => keyDown('j')}>j</button>
+              <button onClick={() => keyDown('k')}>k</button>
+              <button onClick={() => keyDown('l')}>l</button>
+              <button onClick={() => keyDown('m')}>m</button>
               <br/>
-              <button>n</button>
-              <button>o</button>
-              <button>p</button>
-              <button>q</button>
-              <button>r</button>
-              <button>s</button>
-              <button>t</button>
-              <button>u</button>
-              <button>v</button>
-              <button>w</button>
-              <button>x</button>
-              <button>y</button>
-              <button>z</button>
+              <button onClick={() => keyDown('n')}>n</button>
+              <button onClick={() => keyDown('o')}>o</button>
+              <button onClick={() => keyDown('p')}>p</button>
+              <button onClick={() => keyDown('q')}>q</button>
+              <button onClick={() => keyDown('r')}>r</button>
+              <button onClick={() => keyDown('s')}>s</button>
+              <button onClick={() => keyDown('t')}>t</button>
+              <button onClick={() => keyDown('u')}>u</button>
+              <button onClick={() => keyDown('v')}>v</button>
+              <button onClick={() => keyDown('w')}>w</button>
+              <button onClick={() => keyDown('x')}>x</button>
+              <button onClick={() => keyDown('y')}>y</button>
+              <button onClick={() => keyDown('z')}>z</button>
             </div>
           </div>
         </div>
