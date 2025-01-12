31f0989f90ab2a16b903371650c3edb1b267a543
nurkanat-doszhan <nurkanat.doszhan@gmail.com>
2025-01-12 15:50:48 +0500

    Quotes block is deleted

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import About from '../containers/About';
 import Gallery from '../containers/Gallery';
 import Hero from '../containers/Hero';
 import Projects from '../containers/Projects';
-import Quotes from '../containers/Quotes';
 import Stayintouch from '../containers/Stayintouch';
 
 const Home = () => {
@@ -12,7 +11,6 @@ const Home = () => {
       <About />
       <Projects />
       <Gallery />
-      <Quotes />
       <Stayintouch />
     </>
   )
